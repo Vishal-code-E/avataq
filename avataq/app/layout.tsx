9c312752305851c7297e6c6ddf685ba70f883d5b
Vishal <sriram182719@gmail.com>
2026-06-05 15:39:23 +0530

    hydwar

--- avataq/app/layout.tsx
+++ avataq/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="dark" className={inter.variable}>
-      <body>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
